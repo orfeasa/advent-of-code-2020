7dab1209bece1ea0777cd2327ca807a867a1151b
Orfeas Antoniou <orfeas@orfeasa.com>
2020-12-01 18:31:40 +0000

    Optimize solution and include errors

--- day_01/reportRepair.go
+++ day_01/reportRepair.go
@@ -2,60 +2,64 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
 )
 
+// ErrNumNotFound is returned if an input list has no two numbers that sum up to the target
+var ErrNumNotFound = errors.New("cannot find two numbers from input that sum to the target")
+
 // readLines reads a whole file into memory
 // and returns a slice of its lines.
-func readLines(path string) ([]string, error) {
+func readLines(path string) ([]int, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	var lines []string
+	var lines []int
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		num, _ := strconv.Atoi(scanner.Text())
+		lines = append(lines, num)
 	}
 	return lines, scanner.Err()
 }
 
-func sumOfTwoEqualsTo(numbers []string, target int) int {
-	for _, expense1 := range numbers {
-		for _, expense2 := range numbers {
-			exp1, _ := strconv.Atoi(expense1)
-			exp2, _ := strconv.Atoi(expense2)
-			if exp1+exp2 == target {
-				return (exp1 * exp2)
-			}
+func sumOfTwoEqualsTo(numbers []int, target int) (int, error) {
+	hasSeen := make(map[int]bool)
+
+	for _, num := range numbers {
+		need := target - num
+		if hasSeen[need] {
+			return num * need, nil
 		}
+		hasSeen[num] = true
 	}
-	return 0
+	return 0, ErrNumNotFound
 }
 
-func sumOfThreeEqualsTo(numbers []string, target int) int {
-	for _, expense1 := range numbers {
-		for _, expense2 := range numbers {
-			for _, expense3 := range numbers {
-				exp1, _ := strconv.Atoi(expense1)
-				exp2, _ := strconv.Atoi(expense2)
-				exp3, _ := strconv.Atoi(expense3)
-				if exp1+exp2+exp3 == target {
-					return (exp1 * exp2 * exp3)
-				}
-			}
+func sumOfThreeEqualsTo(numbers []int, target int) (int, error) {
+	for ind, num := range numbers {
+		need := target - num
+		mult, err := sumOfTwoEqualsTo(numbers[ind:], need)
+		if err != nil {
+			continue
+		} else {
+			return num * mult, nil
 		}
 	}
-	return 0
+	return 0, ErrNumNotFound
 }
 
 func main() {
 	expenses, _ := readLines("/home/orfeas/code/advent-of-code-2020/day_01/input.txt")
 
-	fmt.Println(sumOfTwoEqualsTo(expenses, 2020))
-	fmt.Println(sumOfThreeEqualsTo(expenses, 2020))
+	res1, _ := sumOfTwoEqualsTo(expenses, 2020)
+	fmt.Println(res1)
+	res2, _ := sumOfThreeEqualsTo(expenses, 2020)
+	fmt.Println(res2)
 }
