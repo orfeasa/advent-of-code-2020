1f08d9ea9e77d57807e03280ca0ad63ab317782c
Orfeas Antoniou <orfeas@orfeasa.com>
2020-12-08 22:42:05 +0000

    Update script to work on all machines and formatting changes

--- run_all.sh
+++ run_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for i in {01..24}
+for i in $(seq -f "%02g" 1 24)
 do
-    FILE=day_$i/main.go
+    FILE="./day_$i/main.go"
     if test -f "$FILE"; then
-        echo "Day $i"
-        go run day_$i/main.go
+        echo "#### Day $i ####"
+        go run ./day_$i/main.go
         printf "\n"
     fi
 done
