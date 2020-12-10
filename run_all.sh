75fe778e5a4a262f15606e4d2a4e5abeff3e1f52
Orfeas Antoniou <orfeas@orfeasa.com>
2020-12-10 17:21:39 +0000

    Minor readability change

--- run_all.sh
+++ run_all.sh
@@ -5,7 +5,7 @@ do
     FILE="./day_$i/main.go"
     if test -f "$FILE"; then
         echo "#### Day $i ####"
-        go run ./day_$i/main.go
+        go run $FILE
         printf "\n"
     fi
 done
