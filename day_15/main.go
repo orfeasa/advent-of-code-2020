11f281f1124725053de913ec80fbd54164572d38
Orfeas Antoniou <orfeas@orfeasa.com>
2020-12-15 13:15:59 +0000

    Refactor to have input in ints

--- day_15/main.go
+++ day_15/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	input := "1,0,16,5,17,4"
+	input := []int{1, 0, 16, 5, 17, 4}
 	fmt.Println("--- Part One ---")
 	fmt.Println(part1(input))
 
@@ -15,26 +13,24 @@ func main() {
 	fmt.Println(part2(input))
 }
 
-func part1(input string) int {
+func part1(input []int) int {
 	return playGameUntil(2020, input)
 }
 
-func part2(input string) int {
+func part2(input []int) int {
 	return playGameUntil(30000000, input)
 }
 
-func playGameUntil(limit int, input string) int {
-	inputStrings := strings.Split(input, ",")
-	// numbers[k] = t means number k was spoken last at time t
+func playGameUntil(limit int, input []int) int {
 	numbers := make(map[int]int)
-	for ind, number := range inputStrings {
-		numbers[toInt(number)] = ind
+
+	for ind, num := range input {
+		numbers[num] = ind
 	}
 
 	last := 0
 	difference := 0
 	isFirstTime := true
-
 	for clock := len(numbers); clock < limit; clock++ {
 		if isFirstTime {
 			last = 0
@@ -55,16 +51,4 @@ func playGameUntil(limit int, input string) int {
 		}
 	}
 	return last
-}
-
-func toInt(s string) int {
-	result, err := strconv.Atoi(s)
-	check(err)
-	return result
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
