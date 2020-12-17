f937bfce176d1143fc84181ac9207903f615badf
Orfeas Antoniou <orfeas@orfeasa.com>
2020-12-17 20:26:39 +0000

    Complete day 17 Due to the current implementation I had to copy each function and change its type to accept 4 dimentions. Thinking of optimizations on that.

--- day_17/main.go
+++ day_17/main.go
@@ -23,15 +23,16 @@ type cube struct {
 
 func part1(inputPath string) int {
 	input := strings.Split(readRaw(inputPath), "\n")
-	cubesActive := make(map[[3]int]bool)
+	const Dimensions = 3
+	cubesActive := make(map[[Dimensions]int]bool)
 	// x increases right, y increases down
 	for y, line := range input {
 		for x, char := range line {
-			z := 0
+			coord := [Dimensions]int{x, y}
 			if char == '#' {
-				cubesActive[[3]int{x, y, z}] = true
+				cubesActive[coord] = true
 			} else {
-				cubesActive[[3]int{x, y, z}] = false
+				cubesActive[coord] = false
 			}
 
 		}
@@ -51,7 +52,34 @@ func part1(inputPath string) int {
 }
 
 func part2(inputPath string) int {
-	return 0
+	input := strings.Split(readRaw(inputPath), "\n")
+	const Dimensions = 4
+	cubesActive := make(map[[Dimensions]int]bool)
+	// x increases right, y increases down
+	for y, line := range input {
+		for x, char := range line {
+			z := 0
+			w := 0
+			if char == '#' {
+				cubesActive[[4]int{x, y, z, w}] = true
+			} else {
+				cubesActive[[4]int{x, y, z, w}] = false
+			}
+
+		}
+	}
+
+	for i := 0; i < 6; i++ {
+		cubesActive = runCycle4d(cubesActive)
+	}
+	count := 0
+	for _, isActive := range cubesActive {
+		if isActive {
+			count++
+		}
+	}
+
+	return count
 }
 
 func runCycle(cubesActive map[[3]int]bool) (nextCubesActive map[[3]int]bool) {
@@ -70,9 +98,36 @@ func runCycle(cubesActive map[[3]int]bool) (nextCubesActive map[[3]int]bool) {
 	return nextCubesActive
 }
 
+func runCycle4d(cubesActive map[[4]int]bool) (nextCubesActive map[[4]int]bool) {
+	nextCubesActive = make(map[[4]int]bool)
+	var neighborsQueue [][4]int
+	for coord := range cubesActive {
+		neighborsQueue = append(neighborsQueue, getNeighbors4d(coord)...)
+		nextCubesActive[coord] = calcNextState4d(coord, cubesActive)
+	}
+	for _, neighborCoords := range neighborsQueue {
+		// if the next state is not already calculated before
+		if _, ok := nextCubesActive[neighborCoords]; !ok {
+			nextCubesActive[neighborCoords] = calcNextState4d(neighborCoords, cubesActive)
+		}
+	}
+	return nextCubesActive
+}
+
 func calcNextState(coord [3]int, cubesActive map[[3]int]bool) bool {
 	isActive := cubesActive[coord]
 	activeNeighborsCount := countActiveNeighbors(coord, cubesActive)
+	if isActive && (activeNeighborsCount == 2 || activeNeighborsCount == 3) {
+		return true
+	} else if !isActive && activeNeighborsCount == 3 {
+		return true
+	}
+	return false
+}
+
+func calcNextState4d(coord [4]int, cubesActive map[[4]int]bool) bool {
+	isActive := cubesActive[coord]
+	activeNeighborsCount := countActiveNeighbors4d(coord, cubesActive)
 	if isActive && (activeNeighborsCount == 2 || activeNeighborsCount == 3) {
 		return true
 	} else if !isActive && activeNeighborsCount == 3 {
@@ -92,6 +147,17 @@ func countActiveNeighbors(coord [3]int, cubesActive map[[3]int]bool) int {
 	return count
 }
 
+func countActiveNeighbors4d(coord [4]int, cubesActive map[[4]int]bool) int {
+	neighbors := getNeighbors4d(coord)
+	count := 0
+	for _, val := range neighbors {
+		if cubesActive[val] {
+			count++
+		}
+	}
+	return count
+}
+
 func getNeighbors(coord [3]int) (neighbors [][3]int) {
 	x0, y0, z0 := coord[0], coord[1], coord[2]
 	for dx := -1; dx <= 1; dx++ {
@@ -99,6 +165,22 @@ func getNeighbors(coord [3]int) (neighbors [][3]int) {
 			for dz := -1; dz <= 1; dz++ {
 				if !(dx == 0 && dy == 0 && dz == 0) {
 					neighbors = append(neighbors, [3]int{x0 + dx, y0 + dy, z0 + dz})
+				}
+			}
+		}
+	}
+	return neighbors
+}
+
+func getNeighbors4d(coord [4]int) (neighbors [][4]int) {
+	x0, y0, z0, w0 := coord[0], coord[1], coord[2], coord[3]
+	for dx := -1; dx <= 1; dx++ {
+		for dy := -1; dy <= 1; dy++ {
+			for dz := -1; dz <= 1; dz++ {
+				for dw := -1; dw <= 1; dw++ {
+					if !(dx == 0 && dy == 0 && dz == 0 && dw == 0) {
+						neighbors = append(neighbors, [4]int{x0 + dx, y0 + dy, z0 + dz, w0 + dw})
+					}
 				}
 			}
 		}
