e9d8e325eb61ba75b65cdb65cabc47033beb66f3
Orfeas Antoniou <orfeas@orfeasa.com>
2020-12-23 10:10:20 +0000

    Linting

--- day_23/main.go
+++ day_23/main.go
@@ -50,11 +50,13 @@ func part2(input string) int {
 	return 0
 }
 
+// Node represents a node of linked list
 type Node struct {
 	value int
 	next  *Node
 }
 
+// LinkedList represents a linked list
 type LinkedList struct {
 	head *Node
 	len  int
