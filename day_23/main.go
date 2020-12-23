6e63f28652cff3650cf7dbde6a0fa0c6f6b50010
Orfeas Antoniou <orfeas@orfeasa.com>
2020-12-23 10:08:50 +0000

    First draft for day 23

---
+++ day_23/main.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	input := "487912365"
+	fmt.Println("--- Part One ---")
+	fmt.Println(part1(input))
+
+	fmt.Println("--- Part Two ---")
+	fmt.Println(part2(input))
+}
+
+func part1(input string) int {
+	// parse input
+	labels := make([]int, 0, len(input))
+	for _, val := range input {
+		labels = append(labels, toInt(string(val)))
+	}
+
+	// generate output
+	reachedOne := false
+	result := 0
+	for _, val := range labels {
+		if val == 1 {
+			reachedOne = true
+		}
+		if reachedOne {
+			result *= 10
+			result += val
+		}
+	}
+	for _, val := range labels {
+		if val == 1 {
+			break
+		}
+		if reachedOne {
+			result *= 10
+			result += val
+		}
+	}
+	return result
+}
+
+func part2(input string) int {
+	return 0
+}
+
+type Node struct {
+	value int
+	next  *Node
+}
+
+type LinkedList struct {
+	head *Node
+	len  int
+}
+
+// InsertAt inserts new node at given position
+func (l *LinkedList) InsertAt(pos int, value int) {
+	// create a new node
+	newNode := Node{}
+	newNode.value = value
+	// validate the position
+	if pos < 0 {
+		return
+	}
+	if pos == 0 {
+		l.head = &newNode
+		l.len++
+		return
+	}
+	if pos > l.len {
+		return
+	}
+	n := l.GetAt(pos)
+	newNode.next = n
+	prevNode := l.GetAt(pos - 1)
+	prevNode.next = &newNode
+	l.len++
+}
+
+// GetAt returns node at given position from linked list
+func (l *LinkedList) GetAt(pos int) *Node {
+	ptr := l.head
+	if pos < 0 {
+		return ptr
+	}
+	if pos > (l.len - 1) {
+		return nil
+	}
+	for i := 0; i < pos; i++ {
+		ptr = ptr.next
+	}
+	return ptr
+}
+
+// Search returns node position with given value from linked list
+func (l *LinkedList) Search(val int) int {
+	ptr := l.head
+	for i := 0; i < l.len; i++ {
+		if ptr.value == val {
+			return i
+		}
+		ptr = ptr.next
+	}
+	return -1
+}
+
+// DeleteAt deletes node at given position from linked list
+func (l *LinkedList) DeleteAt(pos int) error {
+	// validate the position
+	if pos < 0 {
+		fmt.Println("position can not be negative")
+		return errors.New("position can not be negative")
+	}
+	if l.len == 0 {
+		fmt.Println("No nodes in list")
+		return errors.New("No nodes in list")
+	}
+	prevNode := l.GetAt(pos - 1)
+	if prevNode == nil {
+		fmt.Println("Node not found")
+		return errors.New("Node not found")
+	}
+	prevNode.next = l.GetAt(pos).next
+	l.len--
+	return nil
+}
+
+// DeleteVal deletes node having given value from linked list
+func (l *LinkedList) DeleteVal(val int) error {
+	ptr := l.head
+	if l.len == 0 {
+		fmt.Println("List is empty")
+		return errors.New("List is empty")
+	}
+	for i := 0; i < l.len; i++ {
+		if ptr.value == val {
+			if i > 0 {
+				prevNode := l.GetAt(i - 1)
+				prevNode.next = l.GetAt(i).next
+			} else {
+				l.head = ptr.next
+			}
+			l.len--
+			return nil
+		}
+		ptr = ptr.next
+	}
+	fmt.Println("Node not found")
+	return errors.New("Node not found")
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func toInt(s string) int {
+	result, err := strconv.Atoi(s)
+	check(err)
+	return result
+}
