464d96914a868845f9c71b90bb04c89112390f57
Orfeas Antoniou <orfeas@orfeasa.com>
2020-12-23 18:55:49 +0000

    Continue working on day 23 part 1

--- day_23/main.go
+++ day_23/main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"errors"
 	"fmt"
+	"sort"
 	"strconv"
 )
 
 func main() {
-	input := "487912365"
+	// input := "487912365"
+	testInput := "389125467"
 	fmt.Println("--- Part One ---")
-	fmt.Println(part1(input))
+	fmt.Println(part1(testInput))
 
 	fmt.Println("--- Part Two ---")
-	fmt.Println(part2(input))
+	fmt.Println(part2(testInput))
 }
 
+// TODO: check https://golang.org/pkg/container/ring/ and https://github.com/atedja/gring/blob/master/ring.go
 func part1(input string) int {
 	// parse input
 	labels := make([]int, 0, len(input))
@@ -22,32 +25,62 @@ func part1(input string) int {
 		labels = append(labels, toInt(string(val)))
 	}
 
-	// generate output
-	reachedOne := false
-	result := 0
+	var labelsSorted = make([]int, len(input))
+	copy(labelsSorted[:], labels[:])
+	sort.Sort(sort.Reverse(sort.IntSlice(labelsSorted[:])))
+	fmt.Println(labelsSorted)
+
+	// store labels in linked list
+	ll := CircularLinkedList{len: 0}
 	for _, val := range labels {
-		if val == 1 {
-			reachedOne = true
-		}
-		if reachedOne {
-			result *= 10
-			result += val
-		}
+		ll.Insert(val)
 	}
-	for _, val := range labels {
-		if val == 1 {
-			break
+
+	// simulate 100 moves
+	currentCup := ll.head
+	for i := 0; i < 10; i++ {
+		currentCupInd := ll.Search(currentCup.value)
+		fmt.Println("-- move", i+1, "--")
+		fmt.Println("cups:", ll.ListToArray(), ", current cup index:", currentCupInd)
+		// remove and keep value and order of 3 values next to current cup
+		var removed []int
+		for j := 0; j < 3; j++ {
+			val := ll.GetAt(currentCupInd + 1).value
+			removed = append(removed, val)
+			ll.DeleteAt(currentCupInd + 1)
 		}
-		if reachedOne {
-			result *= 10
-			result += val
+		fmt.Println("pick up:", removed)
+
+		// select destination cup
+		destinationVal := getNext(currentCup.value, labelsSorted)
+		destinationPos := ll.Search(destinationVal)
+		for destinationPos == -1 {
+			destinationVal = getNext(destinationVal, labelsSorted)
+			destinationPos = ll.Search(destinationVal)
 		}
+		fmt.Println("destination:", destinationVal, "\n")
+
+		// place cups next to destination
+		for ind, val := range removed {
+			ll.InsertAt(destinationPos+ind+1, val)
+		}
+		currentCup = currentCup.next
 	}
-	return result
+
+	return ll.GenerateIntReprStartingWithVal(1)
 }
 
 func part2(input string) int {
 	return 0
+}
+
+func getNext(find int, a []int) (next int) {
+	for ind, val := range a {
+		if val == find {
+			return a[(ind+1)%len(a)]
+		}
+	}
+	return -1
 }
 
 // Node represents a node of linked list
@@ -56,14 +89,57 @@ type Node struct {
 	next  *Node
 }
 
-// LinkedList represents a linked list
-type LinkedList struct {
+// CircularLinkedList represents a linked list
+type CircularLinkedList struct {
 	head *Node
 	len  int
 }
 
+// GenerateIntReprStartingWithVal generates an int representation of the list that starts after val and goes through the entire list (except for val)
+func (l *CircularLinkedList) GenerateIntReprStartingWithVal(val int) (repr int) {
+	node := l.GetAt(l.Search(val))
+	node = node.next
+	for node.value != val {
+		repr = 10*repr + node.value
+		node = node.next
+	}
+	return repr
+}
+
+// ListToArray prints the circular list without repetition
+func (l *CircularLinkedList) ListToArray() []int {
+	array := make([]int, 0, l.len)
+	current := l.head
+	for i := 0; i < l.len; i++ {
+		array = append(array, current.value)
+		current = current.next
+	}
+	return array
+}
+
+// Insert inserts new node at the end of linked list
+func (l *CircularLinkedList) Insert(val int) {
+	n := Node{value: val, next: l.head}
+	if l.len == 0 {
+		l.head = &n
+		l.head.next = &n
+		l.len++
+		return
+	}
+	ptr := l.head
+	for i := 0; i < l.len; i++ {
+		if ptr.next == l.head {
+			ptr.next = &n
+			ptr.next.next = l.head
+			l.len++
+			return
+		}
+		ptr = ptr.next
+	}
+}
+
 // InsertAt inserts new node at given position
-func (l *LinkedList) InsertAt(pos int, value int) {
+func (l *CircularLinkedList) InsertAt(pos int, value int) {
 	// create a new node
 	newNode := Node{}
 	newNode.value = value
@@ -87,13 +163,10 @@ func (l *LinkedList) InsertAt(pos int, value int) {
 }
 
 // GetAt returns node at given position from linked list
-func (l *LinkedList) GetAt(pos int) *Node {
+func (l *CircularLinkedList) GetAt(pos int) *Node {
 	ptr := l.head
-	if pos < 0 {
-		return ptr
-	}
-	if pos > (l.len - 1) {
-		return nil
+	if pos < 0 || pos > (l.len-1) {
+		pos = pos % l.len
 	}
 	for i := 0; i < pos; i++ {
 		ptr = ptr.next
@@ -102,7 +175,7 @@ func (l *LinkedList) GetAt(pos int) *Node {
 }
 
 // Search returns node position with given value from linked list
-func (l *LinkedList) Search(val int) int {
+func (l *CircularLinkedList) Search(val int) int {
 	ptr := l.head
 	for i := 0; i < l.len; i++ {
 		if ptr.value == val {
@@ -114,7 +187,7 @@ func (l *LinkedList) Search(val int) int {
 }
 
 // DeleteAt deletes node at given position from linked list
-func (l *LinkedList) DeleteAt(pos int) error {
+func (l *CircularLinkedList) DeleteAt(pos int) error {
 	// validate the position
 	if pos < 0 {
 		fmt.Println("position can not be negative")
@@ -135,7 +208,7 @@ func (l *LinkedList) DeleteAt(pos int) error {
 }
 
 // DeleteVal deletes node having given value from linked list
-func (l *LinkedList) DeleteVal(val int) error {
+func (l *CircularLinkedList) DeleteVal(val int) error {
 	ptr := l.head
 	if l.len == 0 {
 		fmt.Println("List is empty")
