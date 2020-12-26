ee2c391af704bb6aa51995c35151023e6ddaea6e
Orfeas Antoniou <orfeas@orfeasa.com>
2020-12-26 12:41:06 +0000

    Convert data structure to doubly linked to decrease insertion time

--- day_23/main.go
+++ day_23/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sort"
 	"strconv"
 )
 
@@ -21,10 +20,6 @@ func part1(input string) int {
 		labels = append(labels, toInt(string(val)))
 	}
 
-	var labelsSorted = make([]int, len(input))
-	copy(labelsSorted[:], labels[:])
-	sort.Sort(sort.Reverse(sort.IntSlice(labelsSorted[:])))
-
 	// store labels in linked list
 	l := CircularLinkedList{len: 0}
 	for _, val := range labels {
@@ -41,9 +36,13 @@ func part1(input string) int {
 			l.DeleteAt(l.Search(currentCup.value) + 1)
 		}
 		// select destination cup
-		destinationVal := getNext(currentCup.value, labelsSorted)
-		for l.Search(destinationVal) == -1 {
-			destinationVal = getNext(destinationVal, labelsSorted)
+		destinationVal := currentCup.value - 1
+		for destinationVal <= 0 || intInSlice(removed, destinationVal) {
+			destinationVal--
+			if destinationVal <= 0 {
+				// assign to max value
+				destinationVal = l.len + len(removed)
+			}
 		}
 		// place cups next to destination
 		for ind, val := range removed {
@@ -52,8 +51,7 @@ func part1(input string) int {
 		currentCup = currentCup.next
 	}
 
-	node := l.GetAt(l.Search(1))
-	node = node.next
+	node := l.GetAt(l.Search(1)).next
 	repr := 0
 	for node.value != 1 {
 		repr = 10*repr + node.value
@@ -66,19 +64,20 @@ func part2(input string) int {
 	return 0
 }
 
-func getNext(find int, a []int) (next int) {
-	for ind, val := range a {
-		if val == find {
-			return a[(ind+1)%len(a)]
+func intInSlice(slice []int, target int) bool {
+	for _, val := range slice {
+		if val == target {
+			return true
 		}
 	}
-	return -1
+	return false
 }
 
 // Node represents a node of linked list
 type Node struct {
 	value int
 	next  *Node
+	prev  *Node
 }
 
 // CircularLinkedList represents a linked list
@@ -93,28 +92,28 @@ func (l *CircularLinkedList) Insert(val int) {
 	if l.len == 0 {
 		l.head = &n
 		l.head.next = &n
+		l.head.prev = &n
 		l.len++
 		return
 	}
-	ptr := l.head
-	for i := 0; i < l.len; i++ {
-		if ptr.next == l.head {
-			ptr.next = &n
-			ptr.next.next = l.head
-			l.len++
-			return
-		}
-		ptr = ptr.next
-	}
+
+	ptr := l.head.prev
+	ptr.next = &n
+	l.head.prev = &n
+	ptr.next.next = l.head
+	l.len++
+	return
 }
 
 // InsertAt inserts new node at given position
 func (l *CircularLinkedList) InsertAt(pos int, value int) {
-	newNode := Node{value: value, next: l.GetAt(pos)}
 	prevNode := l.GetAt(pos - 1)
+	nextNode := l.GetAt(pos)
+	newNode := Node{value: value, next: nextNode, prev: prevNode}
 	prevNode.next = &newNode
+	nextNode.prev = &newNode
 	if pos%l.len == 0 {
-		l.head = prevNode.next
+		l.head = &newNode
 	}
 	l.len++
 }
